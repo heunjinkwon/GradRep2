f8c791bb7c84be445d664f27b008432e97063b1c
heunjinkwon <55481708+heunjinkwon@users.noreply.github.com>
2019-09-19 10:10:58 +0900

    Add files via upload

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -14,7 +14,6 @@ print("Pixel at (0, 0) - Red: {}, Green: {}, Blue: {}".format(r, g, b))
 image[0, 0] = (0, 0, 255)
 (b, g, r) = image[0, 0]
 print("Pixel at (0, 0) - Red: {}, Green: {}, Blue: {}".format(r, g, b))
-
 corner = image[0:100, 0:100]
 cv2.imshow("Corner", corner)
 
